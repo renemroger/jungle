ec4251c1b0e8f9a5e1be07767b95703ab4f538d7
mroger <rene.mroger@gmail.com>
2019-08-22 01:25:43 +0000

    dashboard count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
   def show
+    @number_of_products = Product.all.count
+    @number_of_categories = Category.all.count
   end
 end
